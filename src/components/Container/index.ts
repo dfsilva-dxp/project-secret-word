d554d369da31b0a84086f44c1ce12443e2096407
Daniel Silva <dfsilva.dxp@gmail.com>
2023-01-23 16:22:42 -0300

    feat: add container component

---
+++ src/components/Container/index.ts
@@ -0,0 +1,28 @@
+import styled, { css } from "styled-components";
+
+const Container = styled.div`
+  ${({ theme }) => css`
+    width: 100%;
+    margin: 0 auto;
+    padding: 0 ${theme.spacings.xsmall};
+    position: relative;
+
+    @media (min-width: 576px) {
+      max-width: ${theme.grid.container.sm};
+    }
+    @media (min-width: 768px) {
+      max-width: ${theme.grid.container.md};
+    }
+    @media (min-width: 992px) {
+      max-width: ${theme.grid.container.lg};
+    }
+    @media (min-width: 1200px) {
+      max-width: ${theme.grid.container.xlg};
+    }
+    @media (min-width: 1400px) {
+      max-width: ${theme.grid.container.xxlg};
+    }
+  `}
+`;
+
+export default Container;
