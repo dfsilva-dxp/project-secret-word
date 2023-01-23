e801ad42fc43268efcddf3ba09f41ea3dbcef867
Daniel Silva <dfsilva.dxp@gmail.com>
2023-01-23 16:23:01 -0300

    feat: add content component

---
+++ src/components/Content/index.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Content = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export default Content;
