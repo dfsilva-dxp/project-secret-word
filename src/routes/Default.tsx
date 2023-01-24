d4523bed9b5e35a8bd5c1237af2229202e01f9f8
Daniel Silva <dfsilva.dxp@gmail.com>
2023-01-24 19:19:47 -0300

    feat: add routes

---
+++ src/routes/Default.tsx
@@ -0,0 +1,21 @@
+import { Home } from "page";
+import { Route, Routes } from "react-router-dom";
+import Protected from "./Protected";
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route
+        path="game"
+        element={
+          <Protected isSignedIn={false}>
+            <h1>Game</h1>
+          </Protected>
+        }
+      />
+    </Routes>
+  );
+};
+
+export default Router;
